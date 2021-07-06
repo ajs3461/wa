959d4a5e8f166ee69fdb374193a9c26c1c3a0619
ajs3461 <87005650+ajs3461@users.noreply.github.com>
2021-07-06 16:52:42 +0900

    c++ 패치

---
+++ Aㅏ.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, b, c;
+    cin >> a >> b >> c;
+    for (int l = 0; l < 3; l++) {
+        if (a == -1 || b == -1 || c == -1) {
+            if (a == -1) {
+                cout << sqrt(c * c - b * b);
+            }
+            if (b == -1) {
+                cout << sqrt(c * c - a * a);
+            }
+            if (c == -1) {
+                cout << sqrt(a * a + b * b);
+            }
+            else if (a + b > c) {
+                cout << "Impossible.";
+                continue;
+            }
+                break;
+            }
+        }
+    }   
+}
