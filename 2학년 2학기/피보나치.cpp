86b5f3b2fefe819a4bcc2c647a2a1b5692d320c2
ZION <sju0924@khu.ac.kr>
2021-08-07 16:08:31 +0900

    피보나치 추가

---
+++ 2학년 2학기/피보나치.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <stdio.h>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int A, tlqkf, diqkf = 1, toRl = 1;
+	cin >> A;
+	tlqkf = 1;
+	for (int B = 0; B < A; B++) {
+		toRl++;
+		if (toRl / 2 == 0) {
+			diqkf = tlqkf + 1 + tlqkf + 2;
+			cout << diqkf << "\n";
+		}
+		else if (toRl / 2 == 1) {
+			tlqkf = diqkf;
+			tlqkf = diqkf + 1 + diqkf + 2;
+			cout << tlqkf << "\n";
+			toRl = 1;
+		}
+	}
+}
--- 2학년 2학기/피타고라스 한변 계산기(그저행복).cpp
+++ 2학년 2학기/피타고라스 한변 계산기(그저행복).cpp
@@ -4,26 +4,52 @@
 #include <stack>
 #include <queue>
 #include <cmath>
-#include <stdio.h>
 using namespace std;
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int A, tlqkf, diqkf = 1, toRl = 1;
-	cin >> A;
-	tlqkf = 1;
-	for (int B = 0; B < A; B++) {
-		toRl++;
-		if (toRl / 2 == 0) {
-			diqkf = tlqkf + 1 + tlqkf + 2;
-			cout << diqkf << "\n";
-		}
-		else if (toRl / 2 == 1) {
-			tlqkf = diqkf;
-			tlqkf = diqkf + 1 + diqkf + 2;
-			cout << tlqkf << "\n";
-			toRl = 1;
-		}
-	}
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, b, c, l;//소수 변수 3개 생성
+    l = 1;
+    while (true) {//반복문 3번돌아가는거 만듬
+        switch (l) {
+        case 1:
+            cout << "Triangle #1\n";
+            break;
+        case 2:
+            cout << "Triangle #2\n";
+            break;
+        case 3:
+            cout << "Triangle #3\n";
+            break;
+        }
+        cin >> a >> b >> c;
+        if (a == -1 || b == -1 || c == -1) {//만약 3개의 소수 변수가 -1이라면
+            if (a == -1) {//만약 a가 -1이라면
+                cout << sqrt(c * c - b * b) << "\n";//a의 진짜 숫자 찾기
+                l++;
+                continue;
+            }
+            if (b == -1) {//만약 b가 -1이라면
+                cout << sqrt(c * c - a * a) << "\n";//b의 진짜 숫자 찾기
+                l++;
+                continue;
+            }
+            if (c == -1) {//만약 c가 -1이라면
+                cout << sqrt(a * a + b * b) << "\n";//c의 진짜 숫차 찾기
+                l++;
+                continue;
+            }
+            else if (a + b > c) {//삼각형 만들 수 있는지 없는지 판단
+                if (a + b > !c) {
+                    cout << "Impossible.\n";//만약 안돼면 "불가능"을 출력
+                    l++;
+                    continue;
+                }
+            }
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            }
+        }
+    }
 }
